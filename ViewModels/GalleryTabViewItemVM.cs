bb95379228a74c00a3fe3bb78b763ccc539fd0a5
Isac Kim <isac08270@gmail.com>
2024-11-03 23:06:31 +1100

    added example image files

--- ViewModels/GalleryTabViewItemVM.cs
+++ ViewModels/GalleryTabViewItemVM.cs
@@ -12,13 +12,12 @@ namespace FlipViewBugDemo1.ViewModels {
         public List<ImageCollectionPanelVM> ImageCollectionPanelVMs { get; set; }
 
         public GalleryTabViewItemVM() {
-            // CHANGE: set and adjust each ImageInfo.ImageFilePath accordingly
-            //for (int i = 1; i <= 175; i++) {
-            //    ImageInfo info = new() {
-            //        ImageFilePath = Path.Combine(LOCAL_DIR, "images", "2823006", i.ToString("D3") + ".webp")
-            //    };
-            //    _imageInfos.Add(info);
-            //}
+            for (int i = 1; i <= 7; i++) {
+                ImageInfo info = new() {
+                    ImageFilePath = Path.Combine(LOCAL_DIR, i.ToString() + ".jpeg")
+                };
+                _imageInfos.Add(info);
+            }
             Debug.WriteLine(_imageInfos.Count);
             ImageCollectionPanelVMs = _imageInfos.Select(info => new ImageCollectionPanelVM() { ImageInfo = info }).ToList();
         }
