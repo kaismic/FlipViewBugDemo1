1997124f4dddb13d3db855b141306faa7177caa2
Isac Kim <isac08270@gmail.com>
2024-11-03 22:32:41 +1100

    commented out ImageFilePath inserting part

--- ViewModels/GalleryTabViewItemVM.cs
+++ ViewModels/GalleryTabViewItemVM.cs
@@ -12,13 +12,13 @@ namespace FlipViewBugDemo1.ViewModels {
         public List<ImageCollectionPanelVM> ImageCollectionPanelVMs { get; set; }
 
         public GalleryTabViewItemVM() {
-            // CHANGE: adjust image paths accordingly
-            for (int i = 1; i <= 175; i++) {
-                ImageInfo info = new() {
-                    ImageFilePath = Path.Combine(LOCAL_DIR, "images", "2823006", i.ToString("D3") + ".webp")
-                };
-                _imageInfos.Add(info);
-            }
+            // CHANGE: set and adjust each ImageInfo.ImageFilePath accordingly
+            //for (int i = 1; i <= 175; i++) {
+            //    ImageInfo info = new() {
+            //        ImageFilePath = Path.Combine(LOCAL_DIR, "images", "2823006", i.ToString("D3") + ".webp")
+            //    };
+            //    _imageInfos.Add(info);
+            //}
             Debug.WriteLine(_imageInfos.Count);
             ImageCollectionPanelVMs = _imageInfos.Select(info => new ImageCollectionPanelVM() { ImageInfo = info }).ToList();
         }
