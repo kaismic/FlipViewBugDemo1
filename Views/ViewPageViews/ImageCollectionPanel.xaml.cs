ff9b2d3fd8ae768d09cf8bdcf1fe78861c92860a
Isac Kim <isac08270@gmail.com>
2024-11-08 12:09:28 +1100

    turns out it's not a bug haha

--- Views/ViewPageViews/ImageCollectionPanel.xaml.cs
+++ Views/ViewPageViews/ImageCollectionPanel.xaml.cs
@@ -1,12 +1,23 @@
 using FlipViewBugDemo1.Entities;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace FlipViewBugDemo1.Views.ViewPageViews {
     public sealed partial class ImageCollectionPanel : Grid {
-        public ImageInfo ImageInfo { get; set; }
+
+        public static readonly DependencyProperty ImageInfoProperty = DependencyProperty.Register(
+            nameof(ImageInfo),
+            typeof(ImageInfo),
+            typeof(ImageCollectionPanel),
+            null
+        );
+
+        public ImageInfo ImageInfo {
+            get => (ImageInfo)GetValue(ImageInfoProperty);
+            set => SetValue(ImageInfoProperty, value);
+        }
+
+        //public ImageInfo ImageInfo { get; set; }
 
         public ImageCollectionPanel() {
             InitializeComponent();
